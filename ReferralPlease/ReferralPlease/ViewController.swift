c8d49a1ae7c9c8cbc2c36545334940ebc9c6d449
arfullight <tmimvhuang@gmail.com>
2021-02-18 19:57:44 -0700

    first init

---
+++ ReferralPlease/ReferralPlease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReferralPlease
+//
+//  Created by arfullight on 2/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
