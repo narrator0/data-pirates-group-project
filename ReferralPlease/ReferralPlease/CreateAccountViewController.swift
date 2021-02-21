6153d14a8141ac16246dd13f841bf5d8b61d3b3c
Huichueh(Jessica) Lo <72643524+jlo0507@users.noreply.github.com>
2021-02-21 15:14:32 -0800

    set up create account VC with incomplete front end

---
+++ ReferralPlease/ReferralPlease/CreateAccountViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CreateAccountViewController.swift
+//  ReferralPlease
+//
+//  Created by Jessica Lo on 2/21/21.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.navigationController?.navigationBar.isHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
--- ReferralPlease/ReferralPlease/ViewController.swift
+++ ReferralPlease/ReferralPlease/ViewController.swift
@@ -18,14 +18,29 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.navigationController?.navigationBar.isHidden = true
+
         self.buttonGo.layer.cornerRadius = 5
         textPhoneNumber.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textPhoneNumber.frame.height))
         textPhoneNumber.leftViewMode = .always
         textPassword.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textPassword.frame.height))
         textPassword.leftViewMode = .always
     }
-
+    
+    
+    @IBAction func clickCreateAccountLabel(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let createAccountVC = storyboard.instantiateViewController(withIdentifier: "CreateAccountVC") as? CreateAccountViewController else{
+            assertionFailure("couldn't find vc")
+            return
+        }
+        if let nav = self.navigationController {
+            nav.pushViewController(createAccountVC, animated: true)
+        } else {
+            assertionFailure("no nav controller")
+        }
+    }
+    
 
 }
 
