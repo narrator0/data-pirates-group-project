83685432925135d44bfbe4435524f23bfec5c051
chloevo <chloevo26@gmail.com>
2021-02-21 14:18:28 -0800

    some changes on frontend

--- ReferralPlease/ReferralPlease/ViewController.swift
+++ ReferralPlease/ReferralPlease/ViewController.swift
@@ -12,14 +12,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var textPhoneNumber: UITextField!
     @IBOutlet weak var textPassword: UITextField!
     @IBOutlet weak var buttonGo: UIButton!
-    @IBOutlet weak var clickSignUp: UIButton!
-    @IBOutlet weak var clickResetPassword: UIButton!
+    @IBOutlet weak var clickRegister: UIButton!
     
+    @IBOutlet weak var clickResetPass: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.buttonGo.layer.cornerRadius = 5
+        textPhoneNumber.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textPhoneNumber.frame.height))
+        textPhoneNumber.leftViewMode = .always
+        textPassword.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textPassword.frame.height))
+        textPassword.leftViewMode = .always
     }
 
 
