ab0f44cb3a59149973305980d403c5b802c38140
subyun <50339908+subyun@users.noreply.github.com>
2021-02-21 12:21:32 -0800

    Add files via upload

--- ReferralPlease/ReferralPlease/ViewController.swift
+++ ReferralPlease/ReferralPlease/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textPhoneNumber: UITextField!
+    @IBOutlet weak var textPassword: UITextField!
+    @IBOutlet weak var buttonGo: UIButton!
+    @IBOutlet weak var clickSignUp: UIButton!
+    @IBOutlet weak var clickResetPassword: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.buttonGo.layer.cornerRadius = 5
     }
 
 
