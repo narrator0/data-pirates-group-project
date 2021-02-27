936c5e3e1dbdeb472b2a9946ae43d1da527ce338
subyun <50339908+subyun@users.noreply.github.com>
2021-02-26 22:32:54 -0800

    Add files via upload (#13)
    * Add files via upload
    
    * add first time view controller to project
    
    Co-authored-by: arfullight <tmimvhuang@gmail.com>

---
+++ ReferralPlease/ReferralPlease/SettingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingViewController.swift
+//  ReferralPlease
+//
+//  Created by Subin on 2021/02/26.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    @IBOutlet weak var updateButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.updateButton.layer.cornerRadius = 5
+       
+    }
+
+}
