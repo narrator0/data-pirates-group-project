c2c7fade8ec461375ffa6f4991490a27a274ea9f
chloevo <chloevo26@gmail.com>
2021-02-28 13:26:21 -0800

    add scrolling profile page and tab bar

--- ReferralPlease/ReferralPlease/HomePageViewController.swift
+++ ReferralPlease/ReferralPlease/HomePageViewController.swift
@@ -7,23 +7,56 @@
 
 import UIKit
 
-class HomePageViewController: UIViewController {
+class HomePageViewController: UIViewController, UITableViewDataSource {
+    
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        print("home page view called")
+//        guard let mainTabController = storyboard?.instantiateViewController(withIdentifier: "mainTabController") as? MainTabController else
+//        {
+//            assertionFailure("couldn't find vc")
+//            return
+//        }
+//        mainTabController.selectedViewController = mainTabController.viewControllers?[0]
+//        present(mainTabController, animated: true, completion: nil)
+        
+//        guard let mainTabController = storyboard?.instantiateViewController(withIdentifier: "mainTabController") as? MainTabController else
+//        {
+//            assertionFailure("couldn't find vc")
+//            return
+//        }
+//        mainTabController.selectedViewController = mainTabController.viewControllers?[0]
+//        present(mainTabController, animated: true, completion: nil)
+//        UIImageView(frame: CGRectMake(0, 0, 100, 100))
+
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
     }
-    */
+    func tableView(_ tableView: UITableView,
+                   heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? TableViewCell else {return UITableViewCell()}
+//        cell.layer.cornerRadius = cell.frame.height / 2
+        cell.userName.text = "test user name"
+        cell.userDescription.text = "user description"
+        cell.profileImage.image = UIImage(named: "image1")
+        cell.profileImage.layer.cornerRadius = cell.profileImage.frame.size.width / 2
+        cell.profileImage.clipsToBounds = true
+        cell.profileImage.layer.borderWidth = 1
+        cell.profileImage.layer.borderColor = UIColor.blue.cgColor
+        return cell
+    }
 
 }
--- ReferralPlease/ReferralPlease/ProfilePageViewController.swift
+++ ReferralPlease/ReferralPlease/ProfilePageViewController.swift
@@ -8,22 +8,78 @@
 import UIKit
 
 class ProfilePageViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var aboutView: UIView!
+    @IBOutlet weak var aboutText: UILabel!
+    @IBOutlet weak var requestBtn: UIButton!
+    
+//    @IBOutlet weak var scrollView: UIScrollView!
+//    @IBOutlet weak var userImage: UIImageView!
+//    @IBOutlet weak var topView: UIView!
+//    @IBOutlet weak var aboutView: UIView!
+//    @IBOutlet weak var aboutText: UILabel!
+//    @IBOutlet weak var requestBtn: UIButton!
+    //    @IBOutlet weak var topView: UIView!
+//    @IBOutlet weak var userName: UILabel!
+//    @IBOutlet weak var userDesc: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        let contentWidth = scrollView.bounds.width
+//        let contentHeight = scrollView.bounds.height * 3
+//        scrollView.contentSize = CGSize(width: contentWidth, height: contentHeight)
+//
+        userImage.layer.cornerRadius = userImage.frame.size.width / 2
+        userImage.clipsToBounds = true
+        userImage.layer.borderWidth = 1
+        userImage.layer.borderColor = UIColor.black.cgColor
+
+        topView.layer.cornerRadius = 10
+        topView.layer.shadowOpacity = 0.5
+        topView.layer.shadowPath = UIBezierPath(rect: topView.bounds).cgPath
+        topView.layer.shadowRadius = 8
+        topView.layer.shadowOffset = CGSize(width: 0.0, height: 6.0)
+
+
+        aboutView.layer.cornerRadius = 10
+        aboutView.layer.shadowOpacity = 0.5
+        aboutView.layer.shadowPath = UIBezierPath(rect: aboutView.bounds).cgPath
+        aboutView.layer.shadowRadius = 8
+        aboutView.layer.shadowOffset = CGSize(width: 0.0, height: 6.0)
+        aboutText.text = "Entrepreneur and professor, research interests include computer security, financial fraud, and digital identity. As a professor, I made fundamental contributions to computer security. You can find more information, including articles in the popular press here: https://bob.cs.ucdavis.edu/research.html. And here is a lecture I gave about some of my research in hardware security: http://vimeo.com/23836967. And here is a link to my academic website, which includes my full publication list (in case you are having trouble sleeping: https://bob.cs.ucdavis.edu/publications.html"
+        requestBtn.layer.cornerRadius = 5
+        requestBtn.layer.cornerRadius = 10
+        requestBtn.layer.shadowOpacity = 0.5
+//        requestBtn.layer.shadowPath = UIBezierPath(rect: topView.bounds).cgPath
+        requestBtn.layer.shadowRadius = 5
+        requestBtn.layer.shadowOffset = .zero
+        
+//        table.dataSource = self
+//        table.delegate = self
+//
+//        let headerView = UIView()
+//        headerView.backgroundColor = UIColor.blue
+//        table.tableHeaderView = headerView
 
         // Do any additional setup after loading the view.
     }
     
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 4
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "newCell") ?? UITableViewCell()
+//        cell.textLabel?.text = "Hello"
+//        return cell
+//    }
+    
+    
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
