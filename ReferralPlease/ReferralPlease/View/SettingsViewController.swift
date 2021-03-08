a24f1e291e1b0ef2a25b9a386fa557ddbbc88ad7
subyun <50339908+subyun@users.noreply.github.com>
2021-03-07 23:19:38 -0800

    change features
    change navigation bar color
    change a setting button from an image to a button

--- ReferralPlease/ReferralPlease/View/SettingsViewController.swift
+++ ReferralPlease/ReferralPlease/View/SettingsViewController.swift
@@ -9,26 +9,25 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var phoneText: UITextField!
     @IBOutlet weak var updateBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // for button design
         updateBtn.layer.cornerRadius = 5
         updateBtn.layer.shadowOpacity = 0.5
         updateBtn.layer.shadowRadius = 5
         updateBtn.layer.shadowOffset = .zero
         
-        // Do any additional setup after loading the view.
+        // change the color of the navigation back button
+        self.navigationController?.navigationBar.tintColor = .orange
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func tap(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
     }
-    */
-
+    
 }
