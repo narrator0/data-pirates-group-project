ae5a3409499db83d90a36aa5edebed4126728b51
Justin Lim <juylim@ucdavis.edu>
2021-03-08 21:32:59 -0800

    Notification page fully working

---
+++ ReferralPlease/ReferralPlease/MenteeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenteeTableViewCell.swift
+//  
+//
+//  Created by Justin Lim on 3/8/21.
+//
+
+import UIKit
+
+class MenteeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
