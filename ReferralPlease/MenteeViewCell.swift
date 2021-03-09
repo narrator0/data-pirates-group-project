ffca43ca935f4884f18cc518c8c4b73ee7f1be0f
Justin Lim <juylim@ucdavis.edu>
2021-03-08 17:26:54 -0800

    Finished notification page

---
+++ ReferralPlease/MenteeViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenteeViewCell.swift
+//  ReferralPlease
+//
+//  Created by Justin Lim on 3/7/21.
+//
+
+import UIKit
+
+class MenteeViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ ReferralPlease/ReferralPlease/MenteeViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  MenteeViewCell.swift
+//  ReferralPlease
+//
+//  Created by Justin Lim on 3/6/21.
+//
+
+import Foundation
